09a9db8feb379601dd5220dd46acb013ec1599fe
Karolina Klinik <56517458+karoklinik@users.noreply.github.com>
2023-11-29 15:02:52 +0100

    Unit tests for ProductCubit
    Unit tests for ProductCubit:
    * Checking the initial state.
    * ProductLoading and ProductLoaded emissions when loadProducts is called correctly.
    * Emissions ProductLoading and ProductError on error when calling loadProducts.
    * ProductLoading and ProductLoaded emissions when calling loadLocalProducts correctly.
    * Emissions ProductLoading and ProductError on error when calling loadLocalProducts.

--- test/product_cubit_test.dart
+++ test/product_cubit_test.dart
@@ -1,53 +1,89 @@
-import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:bloc_test/bloc_test.dart';
 import 'package:mockito/mockito.dart';
-import 'package:quick_sell/presentation/cubits/product_cubit.dart';
+import 'package:quick_sell/domain/models/product.dart';
 import 'package:quick_sell/domain/product_repository.dart';
-import 'package:quick_sell/presentation/pages/product_list_page.dart';
+import 'package:quick_sell/presentation/cubits/product_cubit.dart';
+import 'package:mockito/annotations.dart';
 
-class MockProductRepository extends Mock implements ProductRepository {}
+@GenerateMocks([ProductRepository])
+import 'product_cubit_test.mocks.dart';
 
 void main() {
-  group('ProductCubit', () {
-    late ProductCubit productCubit;
-    late MockProductRepository mockRepository;
+  late ProductRepository mockRepository;
+  late ProductCubit productCubit;
 
-    setUp(() {
-      mockRepository = MockProductRepository();
-      productCubit = ProductCubit(mockRepository);
-    });
+  List<Product> mockProducts = [
+    Product('Product A', 29.99),
+    Product('Product B', 59.99),
+  ];
 
-    testWidgets('Initial state is ProductLoading', (WidgetTester tester) async {
-      await tester.pumpWidget(
-        BlocProvider(
-          create: (context) => productCubit,
-          child: const MaterialApp(
-            home: ProductsListPage(),
-          ),
-        ),
-      );
-    });
+  List<Product> mockLocalProducts = [
+    Product('Product C', 19.99),
+    Product('Product D', 519.99),
+  ];
 
-    // test('Emits ProductLoaded when products are loaded', () {
-    //   final products = [
-    //     Product('Product 1', 10.0),
-    //     Product('Product 2', 20.0),
-    //   ];
-
-    //   when(mockRepository.getProducts()).thenAnswer((_) async => products);
-
-    //   productCubit.loadProducts();
-
-    //   expect(productCubit.state, ProductLoaded(products));
-    // });
-
-    // test('Emits ProductError when there is an error', () {
-    //   when(mockRepository.getProducts()).thenThrow(Exception('Error'));
-
-    //   productCubit.loadProducts();
-
-    //   expect(productCubit.state, ProductError());
-    // });
+  setUp(() {
+    mockRepository = MockProductRepository();
+    productCubit = ProductCubit(mockRepository);
   });
+
+  test('Initial state is loading products', () {
+    expect(productCubit.state, const TypeMatcher<ProductLoading>());
+  });
+
+  blocTest<ProductCubit, ProductState>(
+    'Loads all available products successfully',
+    build: () {
+      when(mockRepository.getProducts()).thenAnswer((_) async => mockProducts);
+      return productCubit;
+    },
+    act: (cubit) => cubit.loadProducts(),
+    expect: () => [
+      isA<ProductLoading>(),
+      isA<ProductLoaded>(),
+    ],
+  );
+
+  blocTest<ProductCubit, ProductState>(
+    'Fails to load products due to an error when loading all available products',
+    build: () {
+      when(mockRepository.getProducts())
+          .thenThrow(Exception('Failed to load products'));
+      return productCubit;
+    },
+    act: (cubit) => cubit.loadProducts(),
+    expect: () => [
+      isA<ProductLoading>(),
+      isA<ProductError>(),
+    ],
+  );
+
+  blocTest<ProductCubit, ProductState>(
+    'Loads local products successfully',
+    build: () {
+      when(mockRepository.getLocalProducts())
+          .thenAnswer((_) async => mockLocalProducts);
+      return productCubit;
+    },
+    act: (cubit) => cubit.loadLocalProducts(),
+    expect: () => [
+      isA<ProductLoading>(),
+      isA<ProductLoaded>(),
+    ],
+  );
+
+  blocTest<ProductCubit, ProductState>(
+    'Fails to load local products due to an error when loading local products',
+    build: () {
+      when(mockRepository.getLocalProducts())
+          .thenThrow(Exception('Failed to load local products'));
+      return productCubit;
+    },
+    act: (cubit) => cubit.loadLocalProducts(),
+    expect: () => [
+      isA<ProductLoading>(),
+      isA<ProductError>(),
+    ],
+  );
 }
